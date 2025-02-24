9ace8cd7195fd9d82272573254336ae4b9b1f9e9
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-02-24 21:55:06 +0200

    Add auto save

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const canv = document.getElementById('canvas');
 const btnClear = document.getElementById('clear');
-const btnSave = document.getElementById('save');
 const btnPlay = document.getElementById('play');
 const colorPicker = document.getElementById('color');
 const lineWidthInput = document.getElementById('line-width');
@@ -9,7 +8,9 @@ let isMouseDown = false;
 let ctx = canv.getContext('2d');
 let lineWidth = 10;
 let lineColor = 'black';
-const cords = [];
+let coords = [];
+console.log(JSON.parse(localStorage.getItem('coords')));
+let timer = null;
 
 // set canvas size
 canv.width = window.innerWidth;
@@ -19,21 +20,37 @@ canv.height = window.innerHeight;
 const clearCanvasLayer = () => {
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canv.width, canv.height);
-
   ctx.beginPath();
+};
+
+const resetLayer = () => {
+  clearCanvasLayer();
+
+  clearInterval(timer);
+  localStorage.setItem('coords', JSON.stringify([]));
+
   console.log('Cleared');
 };
-btnClear.addEventListener('click', clearCanvasLayer);
-
-// save
-const saveCanvasLayer = () => {
-  localStorage.setItem('cords', JSON.stringify(cords));
-  console.log('Saved');
-};
-btnSave.addEventListener('click', saveCanvasLayer);
+btnClear.addEventListener('click', resetLayer);
 
 // play
 const playCanvas = () => {
+  coords = JSON.parse(localStorage.getItem('coords'));
+
+  clearCanvasLayer();
+
+  timer = setInterval(() => {
+    if (!coords.length) {
+      clearInterval(timer);
+      ctx.beginPath();
+      console.log('Empty');
+      return;
+    }
+    let crd = coords.shift();
+    let e = { clientX: crd['0'], clientY: crd['1'] };
+
+    drawLine(e);
+  }, 10);
   console.log('Play');
 };
 btnPlay.addEventListener('click', playCanvas);
@@ -48,7 +65,6 @@ lineWidthInput.addEventListener('change', setLineWidth);
 // set line color
 const setFillColor = (e) => {
   const value = e.target.value;
-  console.log(value);
   lineColor = `${value}`;
 };
 colorPicker.addEventListener('change', setFillColor);
@@ -56,30 +72,37 @@ colorPicker.addEventListener('change', setFillColor);
 // set mouseDown
 canv.addEventListener('mousedown', function () {
   isMouseDown = true;
+  coords = JSON.parse(localStorage.getItem('coords'));
 });
 
 canv.addEventListener('mouseup', function () {
   isMouseDown = false;
   ctx.beginPath();
+  coords.push('mouseup');
+  localStorage.setItem('coords', JSON.stringify(coords));
 });
+
+function drawLine(e) {
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = lineColor;
+
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.fillStyle = lineColor;
+  ctx.arc(e.clientX, e.clientY, lineWidth / 2, 0, Math.PI * 2);
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(e.clientX, e.clientY);
+}
 
 // canvas settings
 canv.addEventListener('mousemove', function (e) {
   if (isMouseDown) {
-    cords.push([e.clientX, e.clientY]);
+    coords.push([e.clientX, e.clientY]);
 
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = lineColor;
-
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.fillStyle = lineColor;
-    ctx.arc(e.clientX, e.clientY, lineWidth / 2, 0, Math.PI * 2);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
+    drawLine(e);
   }
 });
